9afab877b22705a0549d73e95fec48ead00ae4aa
Evandabest <evanhaque1@gmail.com>
2026-01-09 17:27:45 -0500

    Initial Commit

---
+++ Almanac-clone/Almanac_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Almanac_cloneApp.swift
+//  Almanac-clone
+//
+//  Created by Evan Haque on 1/9/26.
+//
+
+import SwiftUI
+
+@main
+struct Almanac_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Almanac-clone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Almanac-clone
+//
+//  Created by Evan Haque on 1/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
